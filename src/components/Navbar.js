c387ace787311756c449f8ddcad26f0ce10f3a94
Pinki Jha <pinkijha1310@gmail.com>
2024-09-20 19:20:03 +0530

    create navbar and add some css on it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import logo from "../assets/portfolio-logo.png";
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
     <div>
-      nnn
+      <nav className='navbar'>
+        <Link className='port'>Port<span className='folio'>Folio</span></Link>
+        <div className='desktopMenu'>
+            <Link className=' desktopMenuListItem'>Home </Link>
+            <Link className=' desktopMenuListItem'>About </Link>
+            <Link className=' desktopMenuListItem'>Skills </Link>
+            <Link className=' desktopMenuListItem'>Projects </Link>
+
+        </div>
+        <button className='desktopMenuBtn'>
+            <img className='desktipMenuImg' src="" /> Contact 
+        </button>
+      </nav>
     </div>
   )
 }
