ab2a18b4f57e606b78b11f850287205da9e86433
Pinki Jha <pinkijha1310@gmail.com>
2024-09-26 18:50:40 +0530

    edit About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import bg from "../assets/banner-img.jpg"
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className='aboutMain'> 
+      <div className='aboutSection'>
+        <h1 className='aboutHeading'>About Me</h1>
+        <p className='aboutPara'>Hello! I'm Pinki Jha, a passionate front-end developer with over 2.5 years of experience building responsive and user-friendly web applications.I specialize in React.js, HTML5, CSS3, JavaScript, and have hands-on experience with Bootstrap, Redux, and Tailwind CSS.
+
+        Throughout my career, I’ve worked on a variety of projects, including e-commerce websites and enterprise-level applications, focusing on enhancing user experiences through clean and efficient code. I enjoy transforming complex problems into intuitive, visually appealing designs.<br/>
+
+        <span className='aboutBuildPara'>Let’s build something great together!</span>
+</p>
+      </div>
+
+        <div className='bgImages'>
+            <img className='aboutBg' src={bg} />
+        </div>
     </div>
   )
 }
