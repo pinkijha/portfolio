bf6dba7f933bec4a1b4cf8670212c2cfdeb98cea
Pinki Jha <pinkijha1310@gmail.com>
2024-10-09 15:53:02 +0530

    make menubar responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import { MdOutlineMenu } from "react-icons/md";
 
 const Navbar = () => {
 
+const [humbergerOpen, setHumbergerOpen] = useState(false);
 
+const toggleHumberger = () => {
+  setHumbergerOpen(!humbergerOpen)
+}
  
 
   return (
     <div>
-      <nav   className='navbar'>
+      <nav className='navbar'>
         <Link className='port'>Port<span className='folio'>Folio</span></Link>
-        <div className='desktopMenu'>
+        
+          <div className={`desktopMenu ${humbergerOpen ? 'open' : ''}`} >
+            
             <Link className=' desktopMenuListItem' to="/home" >Home </Link>
             <Link className=' desktopMenuListItem' to="/home" >About </Link>
             <Link className=' desktopMenuListItem' to="/home" >Skills </Link>
             <Link className=' desktopMenuListItem' to="/home" >Projects </Link>
-            {/* <div className=''onClick={showMenu} ><GiHamburgerMenu /></div> */}
 
         </div>
         <button className='desktopMenuBtn'>
              Contact Me
         </button>
+        
+                  
+        <div className='humberger' onClick={toggleHumberger}><MdOutlineMenu /></div>
+         
+        
       </nav>
     </div>
   )
