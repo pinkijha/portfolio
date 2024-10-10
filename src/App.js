0552ff133ae8a24a1760904cafbd0e902dd60e49
Pinki Jha <pinkijha1310@gmail.com>
2024-10-10 19:16:04 +0530

    add project Section and make it mobile responsive

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About";
 import Footer from "./components/Footer";
 import HeaderPage from "./components/HeaderPage";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 
@@ -14,6 +15,7 @@ function App() {
      <HeaderPage/>
      <About/>
      <Skills/>
+     <Projects/>
      <Footer/>
     </div>
   );
---
+++ src/components/Projects.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import kk from '../assets/p7.jpg';
+import dashboard from '../assets/p5.jpg'
+import tyre from '../assets/p8.jpg'
+import calci from '../assets/p3.jpg'
+import ageCalci from '../assets/p6.jpg'
+import ticToc from '../assets/p1.jpg'
+import stonePaper from '../assets/p2.jpg'
+import amazon from '../assets/p4.jpg'
+
+const Projects = () => {
+  return (
+    <div className='projectSection'>
+      <h1 className='projectHeading'>My Projects</h1>
+
+      <div className='projectCrads'>
+        <div className='cardsProjects'>
+            <h2 className='react-heading'>React Projects</h2>
+            <div className='reactImg'>
+                <div className='cards'>  
+                 <a href="https://khao-khilao-pinki-jha.netlify.app/" target="_blank" rel="noopener noreferrer">                     
+                <img className='reactProjectImg' src={kk} />
+                <p>KhaoKhilao App</p>
+                <p>React, Tailwind, Parcel</p>
+                </a>
+                </div>
+
+                
+                <div className='cards'> 
+                <a href="https://dashboard-react-pinki-jha.netlify.app/" target="_blank" rel="noopener noreferrer">                  
+                <img className='reactProjectImg' src={dashboard} /> 
+                <p>Dashboard App</p>
+                <p>React, Tailwind, Parcel</p></a> 
+                </div>
+
+                
+                <div className='cards'>   
+                <a href="https://tyreplex-pinki-jha.netlify.app/" target="_blank" rel="noopener noreferrer">                     
+                <img className='reactProjectImg' src={tyre} />
+                <p>Tyreplex App</p>
+                <p>React, Tailwind, Parcel</p></a>
+                </div>                         
+            </div>
+        </div>
+
+        <div className='cardsProjects'>
+            <h2 className='react-heading'>JavaScript Projects</h2>
+            <div className='reactImg'>
+                <div className='cards'>
+                <a href="https://simple-calculator-v02.netlify.app/" target="_blank" rel="noopener noreferrer">                       
+                <img className='reactProjectImg' src={calci} />
+                <p>Simple Calculator</p>
+                <p>HTML5, CSS3, JavaScript</p></a>
+                </div>
+
+                
+                <div className='cards'>
+                <a href="https://age-calci-app.netlify.app/" target="_blank" rel="noopener noreferrer">                       
+                <img className='reactProjectImg' src={ageCalci} /> 
+                <p>Age Calculator</p>
+                <p>HTML5, CSS3, JavaScript</p></a>
+                </div>
+
+                
+                <div className='cards'>                    
+                <a href="https://tic-tac-toe-app01.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <img className='reactProjectImg' src={ticToc} />
+                <p>Tic Tac Toe</p>
+                <p>HTML5, CSS3, JavaScript</p></a>
+                </div> 
+
+                <div className='cards'>                    
+                <a href="https://stone-paper-scissor-app.netlify.app/" target="_blank" rel="noopener noreferrer">
+                <img className='reactProjectImg' src={stonePaper} />
+                <p>Stone Paper Scissor</p>
+                <p>HTML5, CSS3, JavaScript</p></a>
+                </div> 
+
+            </div>
+        </div>
+
+        <div className='cardsProjects'>
+            <h2 className='react-heading'>HTML5 / CSS3 Projects</h2>
+            <div className='reactImg'>
+                <div className='cards'>  
+                <a href="https://amazon-landing-page1.netlify.app/" target="_blank" rel="noopener noreferrer">                  
+                <img className='reactProjectImg' src={amazon} />
+                <p>Amazon landing Page</p>
+                <p>HTML5, CSS3</p></a>
+                </div>                        
+            </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Projects
