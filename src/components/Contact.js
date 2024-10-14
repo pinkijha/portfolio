146d9cf7b8ee3e16d6cbc8222e2952965b991d56
Pinki Jha <pinkijha1310@gmail.com>
2024-10-14 18:09:32 +0530

    work on contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,15 +4,40 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
+import { IoSend } from 'react-icons/io5'; 
 
 const Contact = () => {
   return (
     <div className='contactSection'>
       <h1 className='contact-heading'>Contact Me</h1>
       <div className='conatct-page'>
-        <div className='contact-card'>
-
+        
+       {/* Contact Card for Form */}
+       <div className='contact-card'>
+          <h2 className='form-title'>Message Me</h2>
+          <form className='contact-form'>
+            <div className='input-group'>
+              <label htmlFor='name'>Name</label>
+              <input type='text' id='name' placeholder='Enter your name' />
+            </div>
+            <div className='input-group'>
+              <label htmlFor='email'>Email</label>
+              <input type='email' id='email' placeholder='Enter your email' />
+            </div>
+            <div className='input-group'>
+              <label htmlFor='subject'>Subject</label>
+              <input type='text' id='subject' placeholder='Enter subject' />
+            </div>
+            <div className='input-group'>
+              <label htmlFor='message'>Describe Project</label>
+              <textarea id='message' rows='5' placeholder='Describe your project here'></textarea>
+            </div>
+            <button type='submit' className='send-btn'>
+              Send Message <IoSend />
+            </button>
+          </form>
         </div>
+
         <div className='conatct-detail'>
             <h3 style={{fontSize: '22px'}}>Reach Out to me!</h3>
             <p>Discuss a Project or just want to say Hi? My inbox is open for all.</p>
